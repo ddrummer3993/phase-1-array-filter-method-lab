969f7a0ccf4df891b9a1a7f1c38d7b9aaefd9c8d
David Drummer <david.b.drummer@gmail.com>
2022-02-07 21:13:43 -0500

    commit tree 1d35acf2a19fe9a6f4e30d3f4094a3fe9183da6a

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Code your solution here
+// test data
+
+const names = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+// create function findMatching
+
+function findMatching(array, string) {
+    let matchingName = array.filter(element => {
+        if (element.toLowerCase() === string.toLowerCase()) {
+            return element;
+        }
+    });
+    return matchingName;
+}
+
+console.log(findMatching(names, 'Sally'));
+
+// creat function fuzzyMatch
+
+function fuzzyMatch(array, string) {
+    let fuzzyName = array.filter(element => {
+        if (element.slice(0, 2) === string) {
+            return element;
+        };
+    });
+    return fuzzyName;
+};
+
+console.log(fuzzyMatch(names, 'Sa'));
+
+
+// creat function matchName
+
+function matchName(array, string) {
+    let matchObj = array.filter(element => {
+        if (element.name === string) {
+            return element;
+        };
+    });
+    return matchObj;
+};
+
+console.log(matchName(drivers, 'Sally'))
